819bc62b2eb11f8055efd21f3639f22f5a2271bc
Danjavia <danjavia.iconshock@gmail.com>
2016-01-24 22:04:22 -0500

    vip changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require( 'path' );
+const node_modules = path.resolve( __dirname, 'node_modules' );
+const pathToReact = path.resolve( node_modules, 'react/dist/react.min.js' );
 
 const PATHS = {
-  	app: path.join(__dirname, 'app'),
-  	build: path.resolve(__dirname, 'build')
+  	app: path.join(__dirname, 'app' ),
+  	build: path.resolve(__dirname, 'build' )
 };
 
 module.exports = {
@@ -11,7 +13,9 @@ module.exports = {
       	'webpack-dev-server/client?http://localhost:8080',
       	path.resolve(__dirname, 'app/main.js')
     ],
-    
+
+    entry: ['webpack/hot/dev-server', path.resolve(__dirname, 'app/main.js')],
+
     output: {
         path: PATHS.build,
         filename: 'bundle.js',
