29b05afb21c1dff22dae4d6a11bb0bacfbe02e70
Danny Viasus(Danjavia) <danny.viasus@comparamejor.com>
2016-01-25 09:39:03 -0500

    revision

--- webpack.config.js
+++ webpack.config.js
@@ -2,41 +2,41 @@ const path = require( 'path' );
 const node_modules = path.resolve( __dirname, 'node_modules' );
 
 const PATHS = {
-  	app: path.join(__dirname, 'app' ),
-  	build: path.resolve(__dirname, 'build' )
+	app: path.resolve(__dirname, 'app/main.js' ),
+	build: path.resolve(__dirname, 'build' )
 };
 
 module.exports = {
 
-    entry: ['webpack/hot/dev-server', path.resolve(__dirname, 'app/main.js')],
+	entry: [ 'webpack/hot/dev-server', PATHS.app ],
 
-    output: {
-        path: PATHS.build,
-        filename: 'bundle.js',
-    },
+	output: {
+		path: PATHS.build,
+		filename: 'bundle.js',
+	},
 
-    module: {
-	  loaders: [
-	    {
-	      test: /\.jsx?$/,
-	      exclude: /(node_modules|bower_components)/,
-	      loader: 'babel', // 'babel-loader' is also a legal name to reference
-	      query: {
-	        presets: [ 'react', 'es2015' ]
-	      }
-	    },
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: 'babel', // 'babel-loader' is also a legal name to reference
+				query: {
+					presets: [ 'react', 'es2015' ]
+				}
+			},
 
-      // LESS
-      {
-        test: /\.less$/,
-        loader: 'style!css!less'
-      },
+			// LESS
+			{
+				test: /\.less$/,
+				loader: 'style!css!less'
+			},
 
-      // SASS
-      {
-        test: /\.scss$/,
-        loader: 'style!css!sass'
-      }
-	  ]
+			// SASS
+			{
+				test: /\.scss$/,
+				loader: 'style!css!sass'
+			}
+		]
 	}
 };
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,23 +1,42 @@
-var path = require('path');
-var node_modules_dir = path.resolve(__dirname, 'node_modules');
+const path = require( 'path' );
+const node_modules = path.resolve( __dirname, 'node_modules' );
 
-var config = {
-  entry: path.resolve(__dirname, 'app/main.js'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel', // 'babel-loader' is also a legal name to reference
-        query: {
-          presets: [ 'react', 'es2015' ]
-        }
-      }]
-  }
+const PATHS = {
+	app: path.resolve(__dirname, 'app/main.js' ),
+	build: path.resolve(__dirname, 'build' )
 };
 
-module.exports = config;
+module.exports = {
+
+	entry: PATHS.app,
+
+	output: {
+		path: PATHS.build,
+		filename: 'bundle.js',
+	},
+
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: 'babel', // 'babel-loader' is also a legal name to reference
+				query: {
+					presets: [ 'react', 'es2015' ]
+				}
+			},
+
+			// LESS
+			{
+				test: /\.less$/,
+				loader: 'style!css!less'
+			},
+
+			// SASS
+			{
+				test: /\.scss$/,
+				loader: 'style!css!sass'
+			}
+		]
+	}
+};
